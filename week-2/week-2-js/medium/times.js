3ccb708a541f7b771280300017dfbec3c1641330
kalpitagrawal <kalpitagrawal06@gmail.com>
2026-01-30 01:18:36 +0530

    Add performance measurement to calculateTime function
    Implement time calculation for summing numbers up to n.

--- week-2/week-2-js/medium/times.js
+++ week-2/week-2-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = performance.now();
+    let sum = 0;
+    for(let i = 1; i <= n; i++){
+        sum += i;
+    }
+    let endTime = performance.now();
+
+    return ((endTime-startTime)/1000) ;
 }
+calculateTime(100);
+calculateTime(1000);
+calculateTime(10000);
