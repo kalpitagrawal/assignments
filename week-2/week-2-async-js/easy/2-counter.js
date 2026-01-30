f08ea730cb34c90df6d8c20f0646a7fd1f7dbc19
kalpitagrawal <kalpitagrawal06@gmail.com>
2026-01-30 11:10:52 +0530

    Implement counter using setTimeout
    Refactor counter implementation to use setTimeout instead of setInterval.

---
+++ week-2/week-2-async-js/easy/2-counter.js
@@ -0,0 +1,77 @@
+/*## Counter without setInterval
+
+Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.*/
+
+
+let count = 0;
+
+function fn() {
+  console.log(++count);
+  setTimeout(fn,1000);
+}
+
+setTimeout(fn,1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
