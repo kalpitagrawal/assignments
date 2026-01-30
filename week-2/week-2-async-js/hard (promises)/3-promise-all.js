0dff2529c895de4678bbcd4e30f6ecba2524ab48
kalpitagrawal <kalpitagrawal06@gmail.com>
2026-01-30 21:09:10 +0530

    Convert calculateTime to async and use Promise.all
    Refactor calculateTime to be async and use Promise.all for concurrent execution of wait functions.

--- week-2/week-2-async-js/hard (promises)/3-promise-all.js
+++ week-2/week-2-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,39 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, t * 1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve) => {
+        setTimeout(resolve, t * 1000);
+    })
 }
-
 function wait3(t) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, t * 1000)
+    })
+}
+
+async function calculateTime(t1, t2, t3) {
+    let startTime = performance.now();
+
+    await Promise.all([wait1(t1), wait2(t2), wait3(t3)]);
+
+    return (performance.now() - startTime);
 
 }
 
-function calculateTime(t1, t2, t3) {
+calculateTime(10, 2, 3).then((time) => {
+    console.log(`Time : ${time/1000} sec`);
+})
 
-}
+
 
 module.exports = calculateTime;
